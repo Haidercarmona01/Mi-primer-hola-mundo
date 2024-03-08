9968df08c6108da95b2f1c79479d316d52390652
Haider Carmona <carmonahaider@gmail.com>
2024-03-08 17:01:51 -0500

    codigo mi primer hola mundo

---
+++ Mi Primer Hola Mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mi_Primer_Hola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
