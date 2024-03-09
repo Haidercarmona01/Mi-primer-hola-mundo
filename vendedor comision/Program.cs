59bd70bcc16e1319968850ba69c11dccb97ea89d
Haider Carmona <carmonahaider@gmail.com>
2024-03-08 20:13:41 -0500

    iniciando proyecto apenas creando proyecto en el repo

---
+++ vendedor comision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vendedor_comision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
