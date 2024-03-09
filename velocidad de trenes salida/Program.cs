3c0dab1807cff3c352424b6acb13b14f5d1b81ed
Haider Carmona <carmonahaider@gmail.com>
2024-03-08 21:25:03 -0500

    Iniciando proyecto de velocidades de tren y probando sincronizacion con github

---
+++ velocidad de trenes salida/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace velocidad_de_trenes_salida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
