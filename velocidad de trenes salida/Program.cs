8e4b493b4386cbcb6e462e2de0d269c92dfe56bf
Haider Carmona <carmonahaider@gmail.com>
2024-03-08 21:55:27 -0500

    calculando las velocidades y cuando se alcanzan

--- velocidad de trenes salida/Program.cs
+++ velocidad de trenes salida/Program.cs
@@ -6,7 +6,22 @@ namespace velocidad_de_trenes_salida
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // anotando la velocidad de los trenes//
+            double velocidadPrimerTren = 80; // km/h
+            double velocidadSegundoTren = 100; // km/h
+
+            // midiendo la distancia recorrida//
+            double distanciaInicial = velocidadPrimerTren; // km
+
+            // Calcular el tiempo  la distancia recorrida por el primer tren para alcanzar al otro//
+            double tiempoAlcance = CalcularTiempoAlcance(distanciaInicial, velocidadSegundoTren); 
+
+            // Calcular la hora que lo alcanza//
+            TimeSpan horaEncuentro = CalcularHoraEncuentro(tiempoAlcance);
+
+            // Mostrar la hora de encuentro del segundo tren//
+            Console.WriteLine($"El segundo tren alcanzará al primer tren a las {horaEncuentro.ToString(@"hh\:mm")}.");
+            Console.ReadLine();
         }
     }
 }
