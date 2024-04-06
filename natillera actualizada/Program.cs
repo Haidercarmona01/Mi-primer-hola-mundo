7b938e9196aef4566f419462ab613fd24afad7f7
Haider Carmona <carmonahaider@gmail.com>
2024-04-05 22:24:31 -0500

    inicializacion del ejercicio donde declaro una variable boleana y unblucle

---
+++ natillera actualizada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+
+namespace natillera_actualizada
+{
+class Program
+{
+    static void Main(string[] args)
+            //imprimir el mensaje de bienvenida 
+    {
+        Console.WriteLine("¡Bienvenido a la Cooperativa Navideña!");
+            // declaracion de una variable boleana
+        bool continuar = true;
+            //Inicia un bucle while que se ejecutará mientras la variable continuar sea verdadera.
+        while (continuar)
+        {
+
+        }
+    }
+}
+
+}
