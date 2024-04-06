f2bd4656fcf81131b33692f81a2a721d2de5c6f6
Haider Carmona <carmonahaider@gmail.com>
2024-04-05 20:39:45 -0500

    primera actualizacion de un odjeto y una variable

---
+++ Juego adivina el numero/Program.cs
@@ -0,0 +1,25 @@
+﻿//Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en
+//adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en
+//memoria sin ser revelado.
+//El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y
+//máximo 4 integrantes. Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generará
+//en los siguientes rangos:
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Esto crea un generador de números aleatorios que se
+        //utilizará para elegir el número que los jugadores deben adivinar.
+        Random random = new Random();
+        bool jugarOtraVez = true;
+
+        //Inicializa una variable  Esta variable controla si el
+        //juego debe repetirse después de que se complete una partida.
+        while (jugarOtraVez)
+        { 
+     }
+}
+}
