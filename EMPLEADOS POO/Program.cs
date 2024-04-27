cf102451442913a352da3ff075d4c4fb6fff2cea
Haidercarmona01 <159094390+Haidercarmona01@users.noreply.github.com>
2024-04-26 21:02:40 -0500

    CREACION DE CLASE Y ATRIBUTOS

---
+++ EMPLEADOS POO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EMPLEADOS_POO
+{
+    //CREACION DE CLASE EMPLEADO 
+    class Empleado
+    {
+        // Atributos NOMBRES EDAD Y TOTAL VENTAS
+        private string nombre;
+        private int edad;
+        private decimal totalVentas;
+    }
+    
+}
